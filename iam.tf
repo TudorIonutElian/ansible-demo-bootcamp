ab46254a204437d2552f968d6ea4faa4f1ff2eb8
ionutelian-tudor <ionut.tudor@consultant.lego.com>
2026-04-28 14:42:02 +0300

    ANSIBLE DEMO: Fix role

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_ssm_role" {
-  name = "EC2RoleWithSSMAccess"
+  name = "EC2RoleForAnsibleDemo"
 
   assume_role_policy = <<EOF
 {
@@ -44,6 +44,6 @@ resource "aws_iam_role_policy" "ec2_revoke_sg" {
 }
 
 resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "ec2_ssm_profile"
+  name = "ec2-ansible-demo-profile"
   role = aws_iam_role.ec2_ssm_role.name
 }
